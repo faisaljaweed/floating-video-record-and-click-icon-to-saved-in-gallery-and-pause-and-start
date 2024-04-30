7ce423fa1d126e142c4037fc859e2423f6e568a8
Faisal Jawed <faisaljawed2000@gmail.com>
2024-04-30 20:22:26 +0500

    floating video recording and click icon video save in gallery and pause and start video

--- lib/create_vid.dart
+++ lib/create_vid.dart
@@ -1,18 +1,10 @@
 import 'package:camera/camera.dart';
 import 'package:floating/floating.dart';
 import 'package:flutter/material.dart';
-import 'package:gallery_saver/gallery_saver.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:recordingcameraa/main.dart';
-
-final aspectRatios = [
-  [1, 1],
-  [2, 3],
-  [3, 2],
-  [16, 9],
-  [9, 16]
-];
 
 class CreateVid extends StatefulWidget {
   const CreateVid({super.key});
@@ -23,28 +15,25 @@ class CreateVid extends StatefulWidget {
 
 class _CreateVidState extends State<CreateVid> {
   late Floating floating;
-  List<int> aspectRation = aspectRatios.first;
-  bool isPipAvailable = false;
-
   late CameraController controller;
   late Future<void> initializeControllerFuture;
-  bool isDisable = false;
+  bool isRecording = false;
   String? path;
+  bool isFullScreen = false;
+  bool isSaving = false; // Flag to indicate if video is being saved
+
   @override
   void initState() {
     super.initState();
     floating = Floating();
-    requestPipAvailabel();
     controller = CameraController(
       cameras[0],
       ResolutionPreset.medium,
     );
-    initializeControllerFuture = controller.initialize();
-  }
-
-  void requestPipAvailabel() async {
-    isPipAvailable = await floating.isPipAvailable;
-    setState(() {});
+    initializeControllerFuture = controller.initialize().then((_) {
+      // Start recording when the controller is initialized
+      _toggleRecording();
+    });
   }
 
   @override
@@ -55,87 +44,122 @@ class _CreateVidState extends State<CreateVid> {
   }
 
   Future<void> _saveVideoToGallery(XFile video) async {
+    setState(() {
+      isSaving = true; // Set saving flag to true
+    });
+
     // Save the video to the device's gallery
     final gallerySaveResult = await GallerySaver.saveVideo(video.path);
     // ignore: avoid_print
     print('Gallery Save Result: $gallerySaveResult'); // For debugging purposes
+
+    setState(() {
+      isSaving = false; // Reset saving flag to false
+    });
   }
 
-  // ignore: annotate_overrides
+  Future<void> _toggleRecording() async {
+    if (isSaving) {
+      return; // If saving, don't start new recording
+    }
+
+    try {
+      await initializeControllerFuture;
+
+      if (!isRecording) {
+        // Start recording
+        path = join(
+          (await getApplicationDocumentsDirectory()).path,
+          '${DateTime.now()}.mp4',
+        );
+        await controller.startVideoRecording();
+        setState(() {
+          isRecording = true;
+        });
+      } else {
+        // Stop recording
+        XFile video = await controller.stopVideoRecording();
+        _saveVideoToGallery(video);
+        setState(() {
+          isRecording = false;
+        });
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return PiPSwitcher(
-      childWhenEnabled: FutureBuilder<void>(
+    return Scaffold(
+      body: PiPSwitcher(
+        childWhenEnabled: Scaffold(
+          body: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 1,
+              child: CameraPreview(controller),
+            ),
+          ),
+        ),
+        childWhenDisabled: FutureBuilder<void>(
           future: initializeControllerFuture,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return Stack(
+              return Column(
                 children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        1, // Set width to screen width
-                    height: MediaQuery.of(context).size.height * 1,
-                    child: CameraPreview(controller),
+                  Stack(
+                    children: [
+                      CameraPreview(controller),
+                    ],
                   ),
-                  // Align(
-                  //   alignment: Alignment.bottomCenter,
-                  //   child: !controller.value.isRecordingVideo
-                  //       ? RawMaterialButton(
-                  //           onPressed: () async {
-                  //             try {
-                  //               await initializeControllerFuture;
-
-                  //               path = join(
-                  //                   (await getApplicationDocumentsDirectory())
-                  //                       .path,
-                  //                   '${DateTime.now()}.mp4');
-
-                  //               setState(() {
-                  //                 controller.startVideoRecording();
-                  //                 isDisable = true;
-                  //                 isDisable = !isDisable;
-                  //               });
-                  //             } catch (e) {
-                  //               // ignore: avoid_print
-                  //               print(e);
-                  //             }
-                  //           },
-                  //           // ignore: sort_child_properties_last
-                  //           child: const Icon(
-                  //             Icons.camera,
-                  //             size: 50.0,
-                  //             color: Colors.yellow,
-                  //           ),
-                  //           padding: const EdgeInsets.all(10.0),
-                  //           shape: const CircleBorder(),
-                  //         )
-                  //       : null,
-                  // ),
-                  // Align(
-                  //     alignment: Alignment.bottomCenter,
-                  //     child: isDisable == !controller.value.isRecordingVideo
-                  //         ? RawMaterialButton(
-                  //             onPressed: () async {
-                  //               setState(() async {
-                  //                 if (controller.value.isRecordingVideo) {
-                  //                   final XFile video =
-                  //                       await controller.stopVideoRecording();
-                  //                   _saveVideoToGallery(video);
-                  //                   // controller.stopVideoRecording();
-                  //                   isDisable = false;
-                  //                   isDisable = !isDisable;
-                  //                 }
-                  //               });
-                  //             },
-                  //             // ignore: sort_child_properties_last
-                  //             child: const Icon(
-                  //               Icons.stop,
-                  //               size: 50.0,
-                  //               color: Colors.red,
-                  //             ),
-                  //             padding: const EdgeInsets.all(10.0),
-                  //             shape: const CircleBorder(),
-                  //           )
-                  //         : null)
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.28,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () async {
+                          if (!isSaving) {
+                            await floating.enable(
+                                aspectRatio: const Rational.landscape());
+                            setState(() {
+                              isFullScreen = true;
+                            });
+                          }
+                        },
+                        tooltip: 'Accessibility Options',
+                        child: const Icon(Icons.videocam),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.1,
+                      ),
+                      FloatingActionButton(
+                        onPressed: () async {
+                          if (!isSaving && controller.value.isRecordingVideo) {
+                            final XFile video =
+                                await controller.stopVideoRecording();
+                            _saveVideoToGallery(video);
+                          }
+                        },
+                        tooltip: 'Accessibility Options',
+                        child: const Icon(Icons.save),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.1,
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {
+                          _toggleRecording();
+                        },
+                        tooltip: 'Accessibility Options',
+                        child: Icon(isRecording ? Icons.pause : Icons.videocam),
+                      ),
+                    ],
+                  ),
                 ],
               );
             } else {
@@ -143,44 +167,8 @@ class _CreateVidState extends State<CreateVid> {
                 child: CircularProgressIndicator(),
               );
             }
-          }),
-      childWhenDisabled: Scaffold(
-        appBar: AppBar(
-          title: const Text("FLoating example app"),
+          },
         ),
-        body: const Center(
-          child: Text("World"),
-        ),
-        floatingActionButton: PiPSwitcher(
-            childWhenEnabled: const Text("Faisal"),
-            childWhenDisabled: FloatingActionButton(
-              onPressed: () async {
-                try {
-                  await initializeControllerFuture;
-
-                  path = join((await getApplicationDocumentsDirectory()).path,
-                      '${DateTime.now()}.mp4');
-
-                  setState(() {
-                    controller.startVideoRecording();
-                    isDisable = true;
-                    isDisable = !isDisable;
-                  });
-                } catch (e) {
-                  // ignore: avoid_print
-                  print(e);
-                }
-                final XFile video = await controller.stopVideoRecording();
-                _saveVideoToGallery(video);
-                // controller.stopVideoRecording();
-                isDisable = false;
-                isDisable = !isDisable;
-                await floating.enable(aspectRatio: const Rational.landscape());
-              },
-              //   onPressed: () async {
-              // await floating.enable(aspectRatio: Rational.landscape());
-              // }
-            )),
       ),
     );
   }
